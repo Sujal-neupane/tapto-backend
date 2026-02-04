305ba17319b27f3912652ec0f41ec81bfdce6daf
sujal neupane <sujalneupane@sujals-MacBook-Pro.local>
2026-02-04 13:29:19 +0545

    did the emailer and notifier and also the email sending

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -10,3 +10,14 @@ export async function connectDatabase(){
         process.exit(1);
     }
 }
+// used as test or mock database can be used connectdatabase and also this test
+// export const connectDBTest = async () => {
+//     const testUri = MONGO_URI + "_test"; // Use a separate test database
+//     try{
+//         await mongoose.connect(testUri);
+//         console.log("MongoDB Test Database connected!");
+//     }catch(error){
+//         console.error("Database error:", error);
+//         process.exit(1); // Exit process with failure
+//     }
+// }
